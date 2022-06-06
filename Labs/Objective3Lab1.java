d8a931d64e38455be0277e50cc16ab652129b6f7
wmconnell <wyatt.connell@gmail.com>
2022-06-06 19:36:15 -0400

    complete Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,11 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 20;
+    num2 = 12;
+    num3 = 11;
+    average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
+  }
+}
