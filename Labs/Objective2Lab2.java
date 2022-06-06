dcf956deee5b863635aef6bcb4c1b629a7ee4551
wmconnell <wyatt.connell@gmail.com>
2022-06-06 19:26:16 -0400

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Wyatt Connell";
+    int age = 28;
+    String state = "Georgia";
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
