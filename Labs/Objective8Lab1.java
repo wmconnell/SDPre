fbe09a85ae97ce899d55809802dff3fe6bb074af
wmconnell <wyatt.connell@gmail.com>
2022-06-10 01:16:49 -0400

    complete Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,10 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for(int i = 1; i < 11; i ++) {
+      System.out.println(i);
+    }
+    for(int j = 10; j > 0; j --) {
+      System.out.println(j);
+    }
+  }
+}
